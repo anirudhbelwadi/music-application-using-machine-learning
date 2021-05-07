2f03efad38cb76247c88e4b3f04fb0687e9101ad
anirudhbelwadi <belwadi.anirudh18@siesgst.ac.in>
2021-05-07 17:07:45 +0400

    added comments in python file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+#import Flask Framework
 from flask import *
 
+#Initialize Flask app
 app = Flask(__name__)
 
+#Root Route
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#Run Flask app when the python file is ran
 if __name__ == "__main__":
     app.run(debug=True)
